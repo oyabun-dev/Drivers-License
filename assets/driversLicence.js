86331392dd8115e328dfd46c72e8856b79f59efc
oyabun-dev <khalifaababacarbeye@esp.sn>
2023-01-17 16:39:55 +0100

    Smooth Scrolling, Go Up & Go Down Button done 🧠😎

--- assets/driversLicence.js
+++ assets/driversLicence.js
@@ -1,12 +1,27 @@
 // @ts-check
 
-function scrolling() {
-    window.scrollTo(0, 700);
-}
+let go_down = document.getElementById('go_down');
+let go_up = document.getElementById('go_up');
 
-// let startMenu = document.getElementById('startMenu');
-// // @ts-ignore
-// startMenu.addEventListener('click', scrolling);
+
+function scrollDown() {
+    window.scrollTo(0, 820);
+    go_down?go_down.toggleAttribute('hidden'):console.error('go_down is null');
+    go_up?go_up.removeAttribute('hidden'):console.error('go_up is null');
+}
+function scrollUp() {
+    window.scrollTo(0, 0);
+    go_up?go_up.toggleAttribute('hidden'):console.error('go_up is null');
+    go_down?go_down.removeAttribute('hidden'):console.error('go_down is null')  ;
+}
+    // @ts-ignore
+go_down.addEventListener('click', scrollDown);
+    // @ts-ignore
+go_up.addEventListener('click', scrollUp);
+
+let getStarted = document.getElementById('getStarted');
+// @ts-ignore
+getStarted.addEventListener('click', scrollDown);
 
 var pageList = [1,2,];
 var pageNumber = 1;
