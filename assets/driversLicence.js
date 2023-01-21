75a806002b1b46a8c0c8586e23f3c663081eabcf
oyabun-dev <khalifaababacarbeye@esp.sn>
2023-01-21 23:00:48 +0100

    All i'am left with is the result page 😎😁

--- assets/driversLicence.js
+++ assets/driversLicence.js
@@ -7,22 +7,22 @@ var type_el_from = document.getElementById('img_container');
 var next = document.getElementById('next');
 var previous = document.getElementById('previous');
 var type = type_el_from?.className;
+var answers = [];
+var pageList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+var pageNumber = pageList[0];
 
-start?.addEventListener('click', function () {
+start?.addEventListener('click', Start);
+next?.addEventListener('click', nextPage);
+previous?.addEventListener('click', previousPage);
+
+function Start () {
     exam_couv?.toggleAttribute('hidden');
     exam_img?.removeAttribute('hidden');
     exam_form?.removeAttribute('hidden');
     // @ts-ignores
     start.style.display = 'none';
     previous?.toggleAttribute('disabled');
-});
-
-var pageList = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
-var pageNumber = pageList[0];
-
-next?.addEventListener('click', nextPage);
-previous?.addEventListener('click', previousPage);
-
+}
 function nextPage(e) {
     e.preventDefault();
     resetCheckbox();
@@ -55,34 +55,22 @@ function previousPage(e) {
         }
     }
 }
-
-// i'm left with only answers collection and the result page
-
-var answers = [];
-
 function saveAnswer() {
-  // Récupère les valeurs des checkboxes cochés
-  var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked'); //[input#A input#C input#D]
-  var checkedValues = Array.from(checkedBoxes).map(function(box) { // [A,C,D] 
-    // @ts-ignore
-    return box.value; //input#A.value = A
-  });
-
-  // Enregistre les réponses pour l'image actuelle
-  answers[pageNumber] = checkedValues; //answers[0] = [A,C,D]
+    var checkedBoxes = document.querySelectorAll('input[type=checkbox]:checked'); //[input#A input#C input#D]
+    var checkedValues = Array.from(checkedBoxes).map(function(box) { // [A,C,D] 
+        // @ts-ignore
+        return box.value; //input#A.value = A
+    });
+    
+    answers[pageNumber] = checkedValues; //answers[1] = [A,C,D]
 }
-
 function resetCheckbox() {
-  // Enregistre les réponses avant de passer à la prochaine image
-  saveAnswer();
-
-  // Réinitialise les checkboxes
-  var checkboxes = document.querySelectorAll('input[type=checkbox]');
-  Array.from(checkboxes).forEach(function(box) {
-    // @ts-ignore
-    box.checked = false;
+    saveAnswer();
+    var checkboxes = document.querySelectorAll('input[type=checkbox]');
+    Array.from(checkboxes).forEach(function(box) {
+        // @ts-ignore
+        box.checked = false;
   });
-//   console.log(pageNumber);
-//   console.log(answers[pageNumber]);
-//   console.log(answers);
 }
+        
+// all i'am left with is the result page
